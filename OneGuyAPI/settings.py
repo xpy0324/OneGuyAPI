67ade893482dca1fc11c00402c7f10e064d2d71e
liujianqiang06 <62226561+liujianqiang06@users.noreply.github.com>
2021-04-09 22:32:43 -0500

    Update settings.py

--- OneGuyAPI/settings.py
+++ OneGuyAPI/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'OneGuyAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends. mysql',
+        'NAME':'first',
+        'USER':'root',
+        'PASSWORD':'1601',
+        'HOST':'47.113.216.240',
+        'PORT':'3306',
+        'OPTIONS':'{'isolation': NONE}'
     }
 }
 
